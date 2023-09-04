ddd0a737d09db23f8ca7284a4fd7d9fa3f2d5539
OBigVee <adedejivictor98@gmail.com>
2023-09-04 12:55:06 +0100

    web serve prepared to serve static contents

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,54 +1,54 @@
 #!/usr/bin/env bash
-# script install and setup nginx web servers for the deployment of web_static
+# sets up your web servers for the deployment of web_static
 
-# update server's repository and install nginx if not already installed
-apt-get update
-apt-get -y install nginx
+# Update servers repository and install nginx
+apt update
+apt -y install nginx
 
-# create necessary directories if they don't exist
+# create project directories
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/releases/test/
 
-# create test html file
-cat << EOF | sudo tee /data/web_static/releases/test/index.html
+# create initial html
+cat << EOF | tee /data/web_static/releases/test/index.html
 <html>
-    <head>
-    </head>
-    <body>
-        Holberton School
-    </body>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
 </html>
 EOF
 
-# create a symbolic linking to release/test/ dir
+# create a symlink to ..release/test/ dir
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # give ownership of /data/ to the current user
 chown -R ubuntu:ubuntu /data/
 
-# set up nginx 
+# set up nginx server configuration
 SERVER=$(hostname)
 
 SERVER_CONFIG="server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
+        listen 80 default_server;
+        listen [::]:80 default_server;
 
-    root /var/www/html;
-    index index.html index.htm index.nginx-debian.html;
+        root /var/www/html;
+        index index.html index.htm index.nginx-debian.html;
 
-    server_name _;
+        server_name _;
 
-    location / {
-            add_header X-Served-By '$SERVER';
-            try_files \$uri \$uri/ =404;
-    }
+        location / {
+                add_header X-Served-By '$SERVER';
+                try_files \$uri \$uri/ =404;
+        }
 
-    location /hbnb_static {
-        add_header X-Served-By '$SERVER';
-        alias /data/web_static/current;
-    }
+	location /hbnb_static {
+                add_header X-Served-By '$SERVER';
+		alias /data/web_static/current;
+	}
 }"
-echo -e "$SERVER_CONFIG" > /etc/nginx/sites-available/default
-systemctl restart nginx
+bash -c "echo -e '$SERVER_CONFIG' > /etc/nginx/sites-available/default"
+/etc/init.d/nginx restart
 
 exit 0
