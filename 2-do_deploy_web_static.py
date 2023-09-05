4039d15dc343ea7907e3905a9fdd7684a5135261
OBigVee <adedejivictor98@gmail.com>
2023-09-05 17:19:25 +0100

    Deploy archive to web servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -29,51 +29,77 @@ def do_pack():
         return None
 
 
+
 def do_deploy(archive_path):
     """Deploys the static files to the host servers.
     Args:
         archive_path (str): The path to the archived static files.
     """
-
-    #  if empty argument passed
     if not os.path.exists(archive_path):
         return False
-
-    basename = os.path.basename(archive_path)
-    path = basename.replace('.tgz', '')
-    path = '/data/web_static/releases/{}'.format(path)
+    file_name = os.path.basename(archive_path)
+    folder_name = file_name.replace(".tgz", "")
+    folder_path = "/data/web_static/releases/{}/".format(folder_name)
     success = False
     try:
-        #  upload archive to server
-        print("##### RUNNING do_deploy function ######\n")
-        print("putting archive in /tmp folder\n")
-        put(archive_path, f'/tmp/{basename}')
-        print("finish /tmp/\n")
-        print("making dir for archive without extension\n")
-        run('sudo mkdir -p {}'.format(path))
-        print("finish making dir without ext\n")
-        print("un-compress archive\n")
-        run('sudo tar -xzf /tmp/{} -C {}'.format(basename, path))
-        print("finish un-compress\n")
-        print("rm /tmp/ folder\n")
-        run(f'sudo rm -rf /tmp/{basename}\n')
-        print("finish removing /tmp\n")
-        print(f"mv {path}/web_static/* to {path}\n")
-        run('sudo mv {}/web_static/* {}'.format(path, path))
-        print("done moving\n")
-        print(f"remove {path}/web_static/\n")
-        run('sudo rm -rf {}/web_static/'.format(path))
-        print("done removing path\n")
-        print("removing /data/web_static/current\n ")
-        run('sudo rm -rf /data/web_static/current')
-        print("done removing /data/web_static/current\n")
-        print("doing linking\n")
-        run('sudo ln -s {} /data/web_static/current'.format(path))
-        print("linking done\n")
+        put(archive_path, "/tmp/{}".format(file_name))
+        run("mkdir -p {}".format(folder_path))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, folder_path))
+        run("rm -rf /tmp/{}".format(file_name))
+        run("mv {}web_static/* {}".format(folder_path, folder_path))
+        run("rm -rf {}web_static".format(folder_path))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {} /data/web_static/current".format(folder_path))
+        print('New version deployed!')
         success = True
     except Exception:
         success = False
     return success
+# def do_deploy(archive_path):
+#     """Deploys the static files to the host servers.
+#     Args:
+#         archive_path (str): The path to the archived static files.
+#     """
+
+#     #  if empty argument passed
+#     if not os.path.exists(archive_path):
+#         return False
+
+#     basename = os.path.basename(archive_path)
+#     path = basename.replace('.tgz', '')
+#     path = '/data/web_static/releases/{}'.format(path)
+#     success = False
+#     try:
+#         #  upload archive to server
+#         print("##### RUNNING do_deploy function ######\n")
+#         print("putting archive in /tmp folder\n")
+#         put(archive_path, f'/tmp/{basename}')
+#         print("finish /tmp/\n")
+#         print("making dir for archive without extension\n")
+#         run('sudo mkdir -p {}'.format(path))
+#         print("finish making dir without ext\n")
+#         print("un-compress archive\n")
+#         run('sudo tar -xzf /tmp/{} -C {}'.format(basename, path))
+#         print("finish un-compress\n")
+#         print("rm /tmp/ folder\n")
+#         run(f'sudo rm -rf /tmp/{basename}\n')
+#         print("finish removing /tmp\n")
+#         print(f"mv {path}/web_static/* to {path}\n")
+#         run('sudo mv {}/web_static/* {}'.format(path, path))
+#         print("done moving\n")
+#         print(f"remove {path}/web_static/\n")
+#         run('sudo rm -rf {}/web_static/'.format(path))
+#         print("done removing path\n")
+#         print("removing /data/web_static/current\n ")
+#         run('sudo rm -rf /data/web_static/current')
+#         print("done removing /data/web_static/current\n")
+#         print("doing linking\n")
+#         run('sudo ln -s {} /data/web_static/current'.format(path))
+#         print("linking done\n")
+#         success = True
+#     except Exception:
+#         success = False
+#     return success
 
 # def do_deploy(archive_path):
 #     """function distributes the files in the archive to web-server"""
