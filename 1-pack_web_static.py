2353790e4efa7b28070ca44633db11a04878784a
OBigVee <adedejivictor98@gmail.com>
2023-09-05 03:33:40 +0100

    compressing before sending task :heavy_check_mark:

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""fabric script generates a .tgz archive from the contents of web_static folder"""
+"""fabric script generates a .tgz archive from the contents of
+web_static folder"""
 
 from datetime import datetime
 from fabric.api import local
@@ -7,9 +8,9 @@ from fabric.api import local
 
 def do_pack():
     """ generate a .tgz archive from all the files in web_static folder"""
-    # run("mkdir /web_static")   
+
     time = datetime.now().strftime("%Y%m%dT%H%M%S")
-        
+
     local("mkdir -p versions")
 
     archive_name = f"web_static_{time}.tgz"
