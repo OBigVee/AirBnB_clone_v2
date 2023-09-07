bc86ff1b51296acd956bb97ad7511403eabaa9cc
OBigVee <adedejivictor98@gmail.com>
2023-09-07 02:26:39 +0100

    script starts flask web app

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""script starts a Flask web application"""
+
+from flask import Flask
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def index():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def Hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def displayC(text):
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route("/python/<text>")
+def python_is_cool(text):
+    return f"Python {text.replace('_', ' ')}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
