1ea5eb1c77eaa2d4fd10a7d6247350992e4158e4
OBigVee <adedejivictor98@gmail.com>
2023-09-07 03:03:57 +0100

    script starts flask web app with number template

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""script starts a Flask web application"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def index():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def Hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>")
+def displayC(text):
+    return f"C {text.replace('_', ' ')}"
+
+
+@app.route("/python")
+@app.route("/python/<text>")
+def python_is_cool(text="is cool"):
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route("/number/<int:n>")
+def is_a_number(n):
+    if isinstance(n, int):
+        return f"{n} is a number"
+
+
+@app.route("/number_template/<int:n>")
+def number_template(n):
+    if isinstance(n, int):
+        return render_template("5-number.html", n=n)
+
+
+@app.route("/number_odd_or_even/<int:n>")
+def odd_or_even_number(n):
+    return render_template("6-number_odd_or_even.html", n=n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
