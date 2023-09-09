f7548c19d1df9806d209ca43942ce2888a8b8682
OBigVee <adedejivictor98@gmail.com>
2023-09-09 20:36:21 +0100

    HBNB is live

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -21,22 +21,24 @@ def index():
     return """welcome to index page"""
 
 
-@app.route('/hbnb')
+@app.route("/hbnb")
 def hbnb_index():
     """AirBnB index page for hbnb_web_static"""
     from models.state import State
     from models.amenity import Amenity
     from models.place import Place
-       
 
     states = storage.all(State).values()
     amenities = storage.all(Amenity).values()
     places = storage.all(Place).values()
 
-    return render_template('100-hbnb.html', states=states,
-                           amenities=amenities,
-                           places=places,
-                           )
+    return render_template(
+        "100-hbnb.html",
+        states=states,
+        amenities=amenities,
+        places=places,
+    )
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
